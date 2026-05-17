2403a36b402f26ad2b39d4ab698fee8612db6806
Ankit Bhandari <ankeetbhandari77@gmail.com>
2026-05-17 20:22:55 +0545

    cleaned up auth folder deleted old register and css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
-import Layout from './components/layout/Layout'
 import Login from './features/auth/Login'
 import Signup from './features/auth/Signup'
-import StaffDirectory from './features/staff/StaffDirectory'
-import FinancialReport from './features/financial/FinancialReport'
-import Inventory from './features/inventory/Inventory'
-
-function PrivateRoute({ children }) {
-  const token = localStorage.getItem('token')
-  return token ? children : <Navigate to="/login" replace />
-}
 
 function App() {
   return (
     <Routes>
       <Route path="/login"  element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
-        <Route index element={<Navigate to="/staff" replace />} />
-        <Route path="staff"     element={<StaffDirectory />} />
-        <Route path="financial" element={<FinancialReport />} />
-        <Route path="inventory" element={<Inventory />} />
-      </Route>
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   )
